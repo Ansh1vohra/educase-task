868f6e82cc246625e969bbd1f78856c604c513af
Ansh1vohra <anshvohra1@gmail.com>
2025-05-13 11:08:58 +0530

    deployment error solving 3

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
-  build: {
-    outDir: 'dist' // default, or change to your custom folder
-  }
 })
